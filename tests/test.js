e1a701ec5b1ebe0368df946d631dfa28074f06b4
Goran Košutić <kosutic.goran@gmail.com>
2017-05-29 12:56:24 +0200

    import manually if possible
    e.g. latest chai-as-promised not compatible with karma-chai, so try to drop things that are not really needed

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,15 @@
+import {expect} from 'chai';
+import sinon from 'sinon';
+
 import example from '../index.js';
 
 describe(`example test`, function () {
     it(`should test something`, function () {
         expect(example()).to.equal(2);
+    });
+    it(`should test with sinon`, function () {
+        const spy = sinon.spy();
+        spy();
+        expect(spy.callCount).to.equal(1);
     });
 });
